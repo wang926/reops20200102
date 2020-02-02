d11bc4307286f516bfb2ae3688c8e58767aba76c
wang926 <812962232@qq.com>
2020-02-02 22:13:03 +0800

    一个新的项目20200202

---
+++ src/main/java/com/weige/Boot200202Application.java
@@ -0,0 +1,13 @@
+package com.weige;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Boot200202Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Boot200202Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/weige/Boot200202ApplicationTests.java
@@ -0,0 +1,16 @@
+package com.weige;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class Boot200202ApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
